fc76b9d0132aa2fe3787301e1e191c34bb423d79
Maksym Yadlovskyi <maksym.yadlovskyi@graylog.com>
2023-09-04 13:05:12 +0200

    fix conflicts

--- graylog2-web-interface/src/components/common/EntityDataTable/EntityDataTable.tsx
+++ graylog2-web-interface/src/components/common/EntityDataTable/EntityDataTable.tsx
@@ -19,7 +19,6 @@ import styled, { css } from 'styled-components';
 import { useMemo, useCallback, useRef } from 'react';
 import type * as Immutable from 'immutable';
 import merge from 'lodash/merge';
-import isFunction from 'lodash/isFunction';
 
 import { Table, ButtonGroup } from 'components/bootstrap';
 import { isPermitted, isAnyPermitted } from 'util/PermissionsMixin';
@@ -243,16 +242,8 @@ const EntityDataTable = <Entity extends EntityBase>({
     displayBulkSelectCol,
     fixedActionsCellWidth,
   });
-
-  const _setSelectedEntities = useCallback((setSelectedEntitiesArgument: SetStateAction<Array<Entity['id']>>) => {
-    const newState = isFunction(setSelectedEntitiesArgument) ? setSelectedEntitiesArgument(selectedEntities) : setSelectedEntitiesArgument;
-
-    setSelectedEntities(newState);
-    if (onChangeSelection) onChangeSelection(newState);
-  }, [onChangeSelection, selectedEntities]);
-
   const onToggleEntitySelect = useCallback((itemId: string) => {
-    _setSelectedEntities(((cur) => {
+    setSelectedEntities(((cur) => {
       if (cur.includes(itemId)) {
         return cur.filter((id) => id !== itemId);
       }
@@ -268,7 +259,7 @@ const EntityDataTable = <Entity extends EntityBase>({
           {displayBulkAction && (
             <BulkActionsRow bulkActions={actions}
                             selectedEntities={selectedEntities}
-                            setSelectedEntities={_setSelectedEntities} />
+                            setSelectedEntities={setSelectedEntities} />
           )}
         </div>
         <LayoutConfigRow>
@@ -290,7 +281,7 @@ const EntityDataTable = <Entity extends EntityBase>({
                      actionsColWidth={actionsColWidth}
                      columnsWidths={columnsWidths}
                      selectedEntities={selectedEntities}
-                     setSelectedEntities={_setSelectedEntities}
+                     setSelectedEntities={setSelectedEntities}
                      data={data}
                      columnRenderersByAttribute={columnRenderersByAttribute}
                      onSortChange={onSortChange}
